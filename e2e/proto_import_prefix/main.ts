6cce44b82b0697c5459096ee5baec49882e436a3
Adam Cozzette <adamc@aspect.build>
2026-03-02 14:26:48 -0800

    Added test

---
+++ e2e/proto_import_prefix/main.ts
@@ -0,0 +1,9 @@
+import { LogMessageSchema } from './proto/logger_pb.js'
+import { create, fromBinary, toBinary } from '@bufbuild/protobuf'
+
+let msg = create(LogMessageSchema, { message: 'hello world' })
+
+// Reference the inherited `.toBinary()` to ensure types from transitive types are included.
+msg = fromBinary(LogMessageSchema, toBinary(LogMessageSchema, msg))
+
+console.log(JSON.stringify(msg))
