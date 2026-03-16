34af71b0ba45405a5899e4c86b7a565981dcbdd4
Adam Cozzette <adamc@aspect.build>
2026-03-16 14:51:56 -0700

    Remove stephenh and timostamm implementations for now

--- examples/protobuf/stephenh-ts-proto/test_proto.js
+++
@@ -1,7 +0,0 @@
-import { User } from '../user.ts'
-import assert from 'node:assert'
-
-const bytes = User.encode({ message: 'hello world' }).finish()
-const user = User.decode(bytes)
-
-assert.equal(user.name, 'hello world')
